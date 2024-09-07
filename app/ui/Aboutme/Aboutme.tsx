4f8e317146bdbd23e1e090517ebef1f56d096a58
Alvaro Barba <alvarobarbafer@gmail.com>
2024-09-07 09:59:09 +0200

    about me projects and technologies update

--- app/ui/Aboutme/Aboutme.tsx
+++ app/ui/Aboutme/Aboutme.tsx
@@ -2,14 +2,28 @@
 
 import React from "react";
 import { jost } from "../fonts";
+import Image from "next/image";
 
 function Aboutme(){
     return(
         <div className="flex flex-col justify-between gap-5">
         <h2 className={`${jost.className} flex justify-start text-6xl px-20 py-4 ml-12 ease out after:w-2/3 after:-scale-x-0 after:origin-left after:border-b-2 after:border-slate-400 after:transition-transform after:duration-1000 after:hover:-scale-x-150`}>About me</h2>
-          <p className="text-wrap pl-12 max-w-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <div className="flex gap-x-96">
+            <p className="text-wrap pl-12 max-w-xl">
+            <span className="quote">&#34;</span>Hi! I&#39;m a passionate software developer who loves exploring new technologies and building meaningful projects.
+            <br />
+            <br />
+            What drives me is the blend of creativity and problem-solving that comes with coding. Whether it&#39;s diving into a complex project or experimenting with new tools, I&#39;m all about continuous learning and making the tech world a bit more exciting.
+            <br />
+            <br />
+            When I&#39;m not coding, you might find me exploring the latest trends in tech or just enjoying some downtime with a good book or game. Let&#39;s connect and create something amazing together!<span className="quote">&#34;</span>
+            </p>
+            <Image className="rounded-sm w-80 h-96" src="/profileSmall.jpg" 
+              alt="Alvaro Barba"
+              width={829}
+              height={940}
+              priority={true}/>
+          </div>
       </div>
     );
 }
--- app/ui/Tech/Tech.tsx
+++ app/ui/Tech/Tech.tsx
@@ -9,7 +9,7 @@ function Tech(){
         <div className="flex flex-col justify-between gap-5">
             <h2 className={`${jost.className} flex justify-start text-6xl px-20 py-4 ml-12 ease out after:w-2/3 after:-scale-x-0 after:origin-left after:border-b-2 after:border-slate-400 after:transition-transform after:duration-1000 after:hover:-scale-x-150`}>Technologies</h2>
                 <p className="text-wrap pl-12 max-w-xl">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <span className="quote">&#34;</span>These are the technologies I love working with and frequently incorporate into my projects to bring ideas to life.<span className="quote">&#34;</span>
                 </p>
             <div className="grid grid-cols-2 p-12 gap-5">
                     <div className="p-3 border-2 border-slate-400 rounded-md">
